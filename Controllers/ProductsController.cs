570f163069bba41a028af46a83a2062c94cc7b9a
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-10-04 08:38:49 +0530

    Controller created

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,26 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MyApiProject.Repository; 
+using MyApiProject.Models;
+using MyApiProject.Services;
 namespace MyApiProject.Controllers
 {
-
-[Route("api/[controller]")]
-[ApiController]
-public class ProductsController : ControllerBase
-{
-    private readonly ProductRepository _productRepository;
-
-    public ProductsController(ProductRepository productRepository)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
     {
-        _productRepository = productRepository;
-    }
+        private readonly IProductService _productservice;
+        public ProductsController(IProductService productService)
+        {
+            _productservice = productService;
+        }
 
-    [HttpGet]
-    public IActionResult GetProducts()
-    {
-        var products = _productRepository.GetProducts();
-        return Ok(products);
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productservice.GetProductAsync();
+            if (products != null)
+            {
+                return Ok(products);
+            }
+            return NotFound("No data present");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById([FromRoute] int id)
+        {
+            var product = await _productservice.GetProductByIdAsync(id);
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound("No Data Found");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct([FromBody] Product product)
+        {
+            await _productservice.AddProductAsync(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.ProductID }, product);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct([FromRoute] int id, [FromBody] Product product)
+        {
+            product.ProductID = id;
+            var updated = await _productservice.UpdateProductAsync(product);
+            if (updated == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct([FromRoute] int id)
+        {
+            var deleted = await _productservice.DeleteProductAsync(id);
+            if (deleted == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
-}
 }
